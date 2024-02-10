7f040fe4d34c7d3480341c5aca9443f9e1988ae8
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2024-02-10 22:48:30 +0530

    React-Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "./components/Navbar"
+
+
+
+function App() {
+  return (
+    <div className='w-full h-screen text-white bg-zinc-900'>
+     <Navbar/>
+  
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full px-20 py-8'>
+      <div className='logo'>
+
+      </div>
+      <div className='links flex gap-10'>
+       {["Services", "Our Work", "About us", "Insights"].map((item, index)=>(
+       <a key={index} className="text-md capitalize font-regular">{item}</a>
+       ))} 
+
+      </div>
+      </div>
+  )
+}
+
+export default Navbar
