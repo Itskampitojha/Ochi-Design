4e3dbfb771b62a463dd0ab7369d439a093d77f59
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2024-02-12 22:47:53 +0530

    Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from "./components/Navbar"
+import LandingPage from "./components/LandingPage"
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='w-full h-screen text-white bg-zinc-900'>
      <Navbar/>
+     <LandingPage/>
   
     </div>
   )
